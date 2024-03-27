9431c5a171ccba782708ce4e131b5af757d91fd9
Art1ku <alihaniskenderbekov@gmail.com>
2024-03-27 20:48:30 +0600

    MSI

---
+++ lesson3/js/appmsi.js
@@ -0,0 +1,94 @@
+var myMSI = 10;
+var b = myMSI +
+10
+console.log(myMSI);
+console.log(b);
+
+("hello");
+console.log(11)
+console.log("11")
+
+var myChip ="You are"
+console.log(myChip)
+var myChip1 ="not me"
+console.log(myChip1)
+
+console.log(myLol)
+var myLol = 10;
+console.log(myLol)
+
+Boolean = true/false
+
+console.log(true)
+console.log(false)
+
+console.log(1 !=2)
+console.log("hello" == "hello ")
+
+console.log(1 == "1")
+
+console.log(1 !== "1")
+
+var a = "a"
+if (a === "a"){
+    console.log("it's true")
+} else{
+    console.log("its false")
+}
+
+//alert("You fucking nigga")
+
+//var mylMSI = prompt("Are you a MSI dealer?")
+
+//console.log(mylMSI)
+
+//if(myName === "Ilya"){
+//    console.log("Super")
+//} else{
+//   console.log("Ti")
+//}
+
+if ("hello"){
+    console.log("true")
+} else{
+    console.log("false")
+}
+
+function myFunc(arg) {
+    console.log("Its a fun")
+
+    if (arg === 4) {
+        console.log("Its 4")
+
+    } else{
+        console.log("Its not 4")
+
+    }
+}
+
+var button = document.querySelector("button")
+console.log(button)
+
+var button = document.querySelector("#button")
+console.log(button)
+
+button.addEventListener("click", function(){
+    console.log("HA")
+    cons
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
