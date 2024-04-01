6fd2f73a4dbce4b2dfc0a64a8f6c450690f251bc
Art1ku <alihaniskenderbekov@gmail.com>
2024-04-01 19:18:07 +0600

    msi

--- lesson3/appmsi.js
+++ lesson3/appmsi.js
@@ -1,7 +1,13 @@
 var mobileMenu = document.querySelector(".mobileMenu")
+var mobileMenuOpened = document.querySelector(".mobileMenuOpened")
 
+console.log(mobileMenuOpened)
 mobileMenu.addEventListener("click", function () {
     console.log("clicked")
+    var menu = document.createElement("div")
+    menu.innerText = "HELLOOOOOOOOOOOOOOOOOOOOOOO"
+    mobileMenuOpened.append(menu)
+    mobileMenuOpened.classList.add('visible')
 })
 
 
