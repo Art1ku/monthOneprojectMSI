209c60720e0cc38eb7f79035bc683b4080017535
Art1ku <alihaniskenderbekov@gmail.com>
2024-06-12 19:59:18 +0600

    lol

--- NewProject/KATANA/src/App.tsx
+++ NewProject/KATANA/src/App.tsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import { Main, Users } from "./pages"
+import { Header } from "./components/Base"
+
 function App() {
   return (
     <>
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}/>
+        <Route path="users" element={<Users />}/>
+      </Routes>
     </>
   )
 }
